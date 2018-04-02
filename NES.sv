e81bb4e89954bd0b10dc13fe6a8009ff393cb92d
GreyRogue <greyrogue@gmail.com>
2018-04-01 22:20:26 -0400

    Switch saves to file images and implement write
    Uses file images (S) instead of file downloads(F) for save files.  When
    a new save file is selected, it is loaded into memory and the NES is
    reset.  When write file is selected, the save is written to the mounted
    image, and the NES is reset.

--- NES.sv
+++ NES.sv
@@ -86,7 +86,7 @@ assign AUDIO_L   = sample;
 assign AUDIO_R   = sample;
 assign AUDIO_MIX = 0;
 
-assign LED_USER  = downloading | (loader_fail & led_blink);
+assign LED_USER  = downloading | saving | (loader_fail & led_blink);
 assign LED_DISK  = 0;
 assign LED_POWER = 0;
 
@@ -102,9 +102,9 @@ assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
 `include "build_id.v"
 parameter CONF_STR = {
 	"NES;;",
-	"-;",
 	"F,NES;",
-	"F,SAV;",
+	"S,SAV;",
+	"T7,Save RAM Write;",
 	"O8,Aspect ratio,4:3,16:9;",
 	"O12,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
 	"O4,Hide overscan,OFF,ON;",
@@ -129,11 +129,24 @@ wire mirroring_osd = status[3];
 wire hide_overscan = status[4];
 wire palette2_osd = status[5];
 wire reset_osd = status[6];
+wire uploading = status[7];
 wire joy_swap = status[9];
 
 wire forced_scandoubler;
 wire ps2_kbd_clk, ps2_kbd_data;
 wire [10:0] ps2_key;
+
+wire saver_mounted;
+wire saver_readonly;
+wire [63:0] saver_size;
+wire [31:0] saver_sd_lba;
+wire saver_sd_rd;
+wire saver_sd_wr;
+wire saver_sd_ack;
+wire [8:0] saver_sd_buff_addr;
+wire [7:0] saver_sd_buff_dout;
+wire [7:0] saver_sd_buff_din;
+wire saver_sd_buff_wr;
 
 hps_io #(.STRLEN(($size(CONF_STR)>>3))) hps_io
 (
@@ -149,22 +162,33 @@ hps_io #(.STRLEN(($size(CONF_STR)>>3))) hps_io
 
    .status(status),
 
+	.img_mounted(saver_mounted),   // signaling that new image has been mounted
+	.img_readonly(saver_readonly), // mounted as read only. valid only for active bit in img_mounted
+	.img_size(saver_size),         // size of image in bytes. valid only for active bit in img_mounted
+
+	// SD block level access
+	.sd_lba(saver_sd_lba),
+	.sd_rd(saver_sd_rd),
+	.sd_wr(saver_sd_wr),
+	.sd_ack(saver_sd_ack),
+	
+	.sd_buff_addr(saver_sd_buff_addr),
+	.sd_buff_dout(saver_sd_buff_dout),
+	.sd_buff_din(saver_sd_buff_din),
+	.sd_buff_wr(saver_sd_buff_wr),
+
 	.ioctl_download(downloading),
 	.ioctl_wr(loader_clk),
 	.ioctl_dout(loader_input),
 	.ioctl_wait(0),
-	.ioctl_index(loader_index),
+	//.ioctl_index(loader_index),
 
    .ps2_key(ps2_key),
 	
 	.ps2_kbd_led_use(0),
 	.ps2_kbd_led_status(0),
 	
-	.sd_lba(0),
-	.sd_rd(0),
-	.sd_wr(0),
-	.sd_conf(0),
-	.sd_buff_din(0)
+	.sd_conf(0)
 );
 
 
@@ -192,7 +216,7 @@ pll pll
 reg [7:0] download_reset_cnt;
 wire download_reset = download_reset_cnt != 0;
 always @(posedge CLK_50M) begin
-	if(downloading) download_reset_cnt <= 8'd255;
+	if(downloading || saving) download_reset_cnt <= 8'd255;
 	else if(download_reset_cnt != 0) download_reset_cnt <= download_reset_cnt - 8'd1;
 end
 
@@ -256,18 +280,45 @@ wire loader_write;
 wire [31:0] loader_flags;
 reg [31:0] mapper_flags;
 wire loader_done, loader_fail;
-wire [7:0] loader_index;
 
 GameLoader loader
 (
 	clk, loader_reset, loader_input, loader_clk, mirroring_osd,
 	loader_addr, loader_write_data, loader_write,
-	loader_flags, loader_done, loader_fail, loader_index
+	loader_flags, loader_done, loader_fail
 );
 
 always @(posedge clk) begin
 	if (loader_done) mapper_flags <= loader_flags;
 end
+
+// Saver
+wire saving;
+wire [21:0] saver_addr;
+wire [7:0] saver_mem_dout;
+wire [7:0] saver_mem_din;
+wire saver_reset; //loader_conf[0];
+wire saver_mem_write;
+wire saver_mem_read;
+wire saver_done;
+wire saver_fail;
+wire saver_rd;
+wire saver_wr;
+assign saving = saver_mounted || !saver_done;
+assign saver_reset = init_reset || downloading || (!saver_mounted && !uploading && saver_done);
+assign saver_rd = saver_mounted;
+assign saver_wr = uploading;
+assign saver_mem_dout = memory_din_ppu; //Saver shares PPU lines
+
+SaveHandler saver
+(
+	clk, saver_reset, saver_rd, saver_wr,
+	run_nes, saver_done, saver_fail,
+	saver_mounted, saver_readonly, saver_size,
+	saver_sd_lba, saver_sd_rd, saver_sd_wr, saver_sd_ack,
+	saver_sd_buff_addr, saver_sd_buff_dout, saver_sd_buff_din, saver_sd_buff_wr,
+	saver_addr, saver_mem_din, saver_mem_write, saver_mem_read, saver_mem_dout
+);
 
 reg led_blink;
 always @(posedge clk) begin
@@ -280,6 +331,7 @@ always @(posedge clk) begin
 end
  
 wire reset_nes = (init_reset || buttons[1] || arm_reset || reset_osd || download_reset || loader_fail);
+//wire run_nes = (!saving && (nes_ce == 3));	// keep running even when reset, so that the reset can actually do its job!
 wire run_nes = (nes_ce == 3);	// keep running even when reset, so that the reset can actually do its job!
 
 // NES is clocked at every 4th cycle.
@@ -302,11 +354,13 @@ NES nes
 assign SDRAM_CKE         = 1'b1;
 
 // loader_write -> clock when data available
+reg loader_read_mem;
 reg loader_write_mem;
 reg [7:0] loader_write_data_mem;
 reg [21:0] loader_addr_mem;
 
 reg loader_write_triggered;
+reg loader_read_triggered;
 
 always @(posedge clk) begin
 	if(loader_write) begin
@@ -315,10 +369,24 @@ always @(posedge clk) begin
 		loader_write_data_mem <= loader_write_data;
 	end
 
+	if(saver_mem_write) begin
+		loader_write_triggered <= 1'b1;
+		loader_addr_mem <= saver_addr;
+		loader_write_data_mem <= saver_mem_din;
+	end
+
+	if(saver_mem_read) begin
+		loader_read_triggered <= 1'b1;
+		loader_addr_mem <= saver_addr;
+	end
+
 	if(nes_ce == 3) begin
 		loader_write_mem <= loader_write_triggered;
 		if(loader_write_triggered)
 			loader_write_triggered <= 1'b0;
+		loader_read_mem <= loader_read_triggered;
+		if(loader_read_triggered)
+			loader_read_triggered <= 1'b0;
 	end
 end
 
@@ -340,15 +408,15 @@ sdram sdram
 	.init         	( !clock_locked     			),
 
 	// cpu/chipset interface
-	.addr     		( downloading ? {3'b000, loader_addr_mem} : {3'b000, memory_addr} ),
+	.addr     		( downloading || saving	? {3'b000, loader_addr_mem} : {3'b000, memory_addr} ),
 	
 	.we       		( memory_write || loader_write_mem	),
-	.din       		( downloading ? loader_write_data_mem : memory_dout ),
+	.din       		( downloading || saving ? loader_write_data_mem : memory_dout ),
 	
 	.oeA         	( memory_read_cpu ),
 	.doutA       	( memory_din_cpu	),
 	
-	.oeB         	( memory_read_ppu ),
+	.oeB         	( memory_read_ppu || loader_read_mem ),
 	.doutB       	( memory_din_ppu	)
 );
 
@@ -405,8 +473,7 @@ module GameLoader
 	output        mem_write,
 	output [31:0] mapper_flags,
 	output reg    done,
-	output reg    error,
-	input [7:0]   file_index
+	output reg    error
 );
 
 reg [1:0] state = 0;
@@ -458,11 +525,10 @@ assign mapper_flags = {15'b0, ines[6][3], has_chr_ram, ines[6][0] ^ invert_mirro
   
 always @(posedge clk) begin
 	if (reset) begin
-		state <= file_index[0]?0:2;
+		state <= 0;
 		done <= 0;
 		ctr <= 0;
-		mem_addr <= file_index[0]?0:22'b11_1100_0000_0000_0000_0000;  // Address for RAM:PRG
-		bytes_left <= 16'h8000;
+		mem_addr <= 0;  // Address for PRG
 	end else begin
 		case(state)
 		// Read 16 bytes of ines header
@@ -496,4 +562,320 @@ always @(posedge clk) begin
 		endcase
 	end
 end
+endmodule
+
+module SaveHandler
+(
+	input         clk,
+	input         reset,
+	input         rd_req,          // i/o read
+	input         wr_req,          // i/o write
+	input         indata_clk,
+	output reg    done,
+	output reg    error,
+
+	// SD config
+	input             img_mounted,  // signaling that new image has been mounted
+	input             img_readonly, // mounted as read only. valid only for active bit in img_mounted
+	input      [63:0] img_size,     // size of image in bytes. valid only for active bit in img_mounted
+
+	// SD block level access
+	output     [31:0] sd_lba,
+	output            sd_rd,       // only single sd_rd can be active at any given time
+	output            sd_wr,       // only single sd_wr can be active at any given time
+	input             sd_ack,
+	
+	// SD byte level access. Signals for 2-PORT altsyncram.
+	input      [AW:0] sd_buff_addr,
+	input      [DW:0] sd_buff_dout,
+	output     [DW:0] sd_buff_din,
+	input             sd_buff_wr,
+
+	// RAM access
+	output reg [21:0] buff_addr,	  // buffer RAM address
+	output      [7:0] buff_dout,	  // buffer RAM data output
+	output            mem_write,    // buffer RAM read enable
+	output            mem_read,     // buffer RAM write enable
+	input       [7:0] buff_din      // buffer RAM data input
+);
+reg [2:0] state = 0;
+reg [10:0] bytes_left;   // One extra bit
+reg [7:0] sectors_left;  // One extra bit
+reg [6:0] buffer_lba;
+wire sd_busy;
+reg sd_save = 0;
+reg sd_read = 0;
+reg rd = 0;
+reg wr = 0;
+reg old_rd = 0;
+reg old_wr = 0;
+reg old_mounted;
+reg old_readonly;
+reg [63:0] old_size;
+reg wrclk;
+wire buff_wr;
+
+localparam WIDE = 0;
+localparam DW = (WIDE) ? 15 : 7;
+localparam AW = (WIDE) ?  7 : 8;
+
+sd_card sd_image
+(
+	.clk(clk),
+	.reset(reset),
+	
+	.sd_lba(sd_lba),
+	.sd_rd(sd_rd),
+	.sd_wr(sd_wr),
+	.sd_ack(sd_ack),
+
+	.sd_buff_addr(sd_buff_addr),
+	.sd_buff_dout(sd_buff_dout),
+	.sd_buff_din(sd_buff_din),
+	.sd_buff_wr(sd_buff_wr),
+
+	.save_sector(sd_save),
+	.read_sector(sd_read),
+	.req_lba(buffer_lba),
+	.buff_addr(buff_addr[8:0]),
+	.buff_dout(buff_dout),
+	.buff_din(buff_din),
+	.buff_we(buff_wr),
+	.busy(sd_busy)
+);
+
+assign mem_write = (bytes_left != 0) && (state == 6) && indata_clk && wrclk;
+assign mem_read = (bytes_left != 0) && (state == 3) && !wrclk;
+assign buff_wr = (bytes_left != 0) && (state == 3) && wrclk;
+  
+always @(posedge clk) begin
+	if (reset) begin
+		state <= 0;
+		done <= 1;
+		error <= 0;
+
+		rd <= 0;
+		wr <= 0;
+		old_rd <= 0;
+		old_wr <= 0;
+		old_mounted <= img_mounted;
+		//old_readonly <= 0;
+		//old_size <= 64'h0;
+		bytes_left <=0;
+		sd_save <= 0;
+		sd_read <= 0;
+		buff_addr <= 22'b11_1100_0000_0000_0000_0000;  // Address for RAM
+		wrclk <= 0;
+		bytes_left <= 11'h000;
+		sectors_left <= 8'h00;
+		buffer_lba <= 7'h0;
+		
+	end else begin
+
+		old_rd <= rd_req;
+		old_wr <= wr_req;
+		old_mounted <= img_mounted;
+		if (~old_mounted && img_mounted) begin
+		  old_readonly <= img_readonly;
+		  old_size <= img_size;
+		end;
+		
+		case(state)
+		0: begin
+			  if (~old_wr && wr_req && ~img_readonly && ((old_size == 16'h8000) || (old_size == 16'h2000))) begin
+			    wr <= 1;
+			    state <= 1;
+			  end else
+			  if (~old_rd && rd_req && ~img_readonly && ((old_size == 16'h8000) || (old_size == 16'h2000))) begin
+			    rd <= 1;
+			    state <= 1;
+			  end;
+			end
+		1: if ((wr) || (rd)) begin
+			  buff_addr <= 22'b11_1100_0000_0000_0000_0000;  // Address for RAM
+			  bytes_left <= 11'h000;
+			  sectors_left <= {1'b0,old_size[15:9]};
+			  state <= img_mounted ? 1: 2; // Wait for notification clear
+			  done <= 0;
+			end
+		2: if (sectors_left == 0) begin
+			  state <= 7;
+			end else begin
+			  bytes_left <= 11'h200;
+			  buffer_lba <= buff_addr[15:9];
+			  if ((wr) && (indata_clk)) begin
+			    wrclk <= 0;
+			    state <= 3;
+			  end else if (rd) begin
+			    sd_read <= 1;
+			    state <= 5;
+			  end
+			end
+		3: begin // Read the next |bytes_left| bytes from |mem_addr|
+			 if (bytes_left != 0) begin
+				if (indata_clk) begin
+				  wrclk <= ~wrclk;
+				  if (wrclk) begin
+				    bytes_left <= bytes_left - 1'd1;
+				    buff_addr <= buff_addr + 1'd1;
+				  end
+				end
+			 end else begin
+				sd_save <= 1;
+				state <= 4;
+			 end
+			end
+		4: begin
+			 if (sd_busy) begin
+			  sd_save <= 0;
+			 end else if (!sd_save) begin
+		     sectors_left <= sectors_left - 1'd1;
+			  state <= 2;
+			 end
+			end
+		5: begin
+			 if (sd_busy) begin
+			  sd_read <= 0;
+			 end else if ((!sd_read) && (indata_clk)) begin
+			  wrclk <= 0;
+			  state <= 6;
+			 end
+			end
+		6: begin // Read the next |bytes_left| bytes into |mem_addr|
+			 if (bytes_left != 0) begin
+				if (indata_clk) begin
+				  wrclk <= ~wrclk;
+				  if (wrclk) begin
+				    bytes_left <= bytes_left - 1'd1;
+				    buff_addr <= buff_addr + 1'd1;
+				  end
+				end
+			 end else begin
+			  sectors_left <= sectors_left - 1'd1;
+			  state <= 2;
+			 end
+			end
+		7: begin
+			 done <= 1;
+			 state <= 0;
+			 rd <= 0;
+			 wr <= 0;
+			end
+		endcase
+	end
+end
+endmodule
+
+module sd_card
+(
+	input         clk,
+	input         reset,
+
+	output [31:0] sd_lba,
+	output reg    sd_rd,
+	output reg    sd_wr,
+	input         sd_ack,
+
+	input   [8:0] sd_buff_addr,
+	input   [7:0] sd_buff_dout,
+	output  [7:0] sd_buff_din,
+	input         sd_buff_wr,
+
+	input         save_sector,
+	input         read_sector,
+	input   [6:0] req_lba,
+	input   [8:0] buff_addr,
+	output  [7:0] buff_dout,
+	input   [7:0] buff_din,
+	input         buff_we,
+	output reg    busy
+);
+
+assign sd_lba = lba;
+
+sd_dpram buffer_dpram
+(
+	.clock(clk),
+
+	.address_a(sd_buff_addr),
+	.data_a(sd_buff_dout),
+	.wren_a(sd_ack & sd_buff_wr),
+	.q_a(sd_buff_din),
+
+	.address_b(buff_addr),
+	.data_b(buff_din),
+	.wren_b(buff_we),
+	.q_b(buff_dout)
+);
+
+reg [31:0] lba;
+
+always @(posedge clk) begin
+	reg old_ack;
+
+	old_ack <= sd_ack;
+	if(sd_ack) {sd_rd,sd_wr} <= 0;
+
+	if(reset) begin
+		busy  <= 0;
+		sd_rd <= 0;
+		sd_wr <= 0;
+	end
+	else
+	if(busy) begin
+		if(old_ack && ~sd_ack) begin
+			busy <= 0;
+		end
+	end
+	else
+	if(save_sector) begin
+		lba <= {25'd0, req_lba};
+		sd_wr <= 1;
+		busy <= 1;
+	end
+	else
+	if(read_sector) begin
+		lba <= {25'd0, req_lba};
+		sd_rd <= 1;
+		busy <= 1;
+	end
+end
+
+endmodule
+
+module sd_dpram #(parameter DATAWIDTH=8, ADDRWIDTH=9)
+(
+	input	                     clock,
+
+	input	     [ADDRWIDTH-1:0] address_a,
+	input	     [DATAWIDTH-1:0] data_a,
+	input	                     wren_a,
+	output reg [DATAWIDTH-1:0] q_a,
+
+	input	     [ADDRWIDTH-1:0] address_b,
+	input	     [DATAWIDTH-1:0] data_b,
+	input	                     wren_b,
+	output reg [DATAWIDTH-1:0] q_b
+);
+
+logic [DATAWIDTH-1:0] ram[0:(1<<ADDRWIDTH)-1];
+
+always_ff@(posedge clock) begin
+	if(wren_a) begin
+		ram[address_a] <= data_a;
+		q_a <= data_a;
+	end else begin
+		q_a <= ram[address_a];
+	end
+end
+
+always_ff@(posedge clock) begin
+	if(wren_b) begin
+		ram[address_b] <= data_b;
+		q_b <= data_b;
+	end else begin
+		q_b <= ram[address_b];
+	end
+end
+
 endmodule
