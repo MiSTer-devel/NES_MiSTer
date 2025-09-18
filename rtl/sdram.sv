b03c76ec4aa5d8c7fe7fbe6e500e361beabaf1b2
Jeferson Chaves <jefchaves@gmail.com>
2025-09-18 20:05:18 -0300

    Fix Verilog-2001 violation: enforce standard-compliant inout handling.
    Fixing a Verilog-2001 standard violation that breaks simulation, synthesis, and formal verification in standards-compliant tools:
    Bidirectional ports (inout) must be driven using continuous assignments (assign), which are only valid for nets (e.g., wire). Using inout reg is explicitly disallowed. Tri-state and bidirectional ports must therefore be implemented with continuous assignments on nets. By contrast, reg types are intended for procedural assignments (inside always blocks), which cannot synthesize proper tri-state drivers for I/O pads.

--- rtl/sdram.sv
+++ rtl/sdram.sv
@@ -23,7 +23,7 @@ module sdram
 (
 
 	// interface to the MT48LC16M16 chip
-	inout  reg [15:0] SDRAM_DQ,   // 16 bit bidirectional data bus
+	inout      [15:0] SDRAM_DQ,   // 16 bit bidirectional data bus
 	output reg [12:0] SDRAM_A,    // 13 bit multiplexed address bus
 	output reg        SDRAM_DQML, // byte mask
 	output reg        SDRAM_DQMH, // byte mask
@@ -65,6 +65,8 @@ module sdram
 	input             ss_load,
 	output     [15:0] ss_out
 );
+
+reg SDRAM_DQ_CONTROL;
 
 assign SDRAM_nCS = 0;
 assign SDRAM_CKE = 1;
@@ -200,6 +202,8 @@ localparam CMD_LOAD_MODE       = 3'b000;
 
 wire [1:0] dqm = {we & ~a[0], we & a[0]};
 
+assign SDRAM_DQ = (SDRAM_DQ_CONTROL)? data: {16{1'bZ}};
+
 // SDRAM state machines
 always @(posedge clk) begin
 	reg [15:0] last_data[3];
@@ -207,10 +211,14 @@ always @(posedge clk) begin
 
 	if(state == STATE_START) SDRAM_BA <= (mode == MODE_NORMAL) ? bank : 2'b00;
 
-	SDRAM_DQ <= 'Z;
+	if({ram_req,we,mode,state} == {2'b11, MODE_NORMAL, STATE_CONT })
+		SDRAM_DQ_CONTROL <= 1'b1;
+	else
+		SDRAM_DQ_CONTROL <= 1'b0;
+	
 	casex({ram_req,we,mode,state})
 		{2'b1X, MODE_NORMAL, STATE_START}: {SDRAM_nRAS, SDRAM_nCAS, SDRAM_nWE} <= CMD_ACTIVE;
-		{2'b11, MODE_NORMAL, STATE_CONT }: {SDRAM_nRAS, SDRAM_nCAS, SDRAM_nWE, SDRAM_DQ} <= {CMD_WRITE, data};
+		{2'b11, MODE_NORMAL, STATE_CONT }: {SDRAM_nRAS, SDRAM_nCAS, SDRAM_nWE} <= {CMD_WRITE};
 		{2'b10, MODE_NORMAL, STATE_CONT }: {SDRAM_nRAS, SDRAM_nCAS, SDRAM_nWE} <= CMD_READ;
 		{2'b0X, MODE_NORMAL, STATE_START}: {SDRAM_nRAS, SDRAM_nCAS, SDRAM_nWE} <= CMD_AUTO_REFRESH;
 
